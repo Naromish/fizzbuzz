56bc0e5145f47200c8249e5427531245aef36045
Naromish <danermc@gmail.com>
2015-06-07 14:15:50 -0400

    initial

---
+++ js/fb.js
@@ -0,0 +1,32 @@
+//$(document).ready(function() {
+
+//});
+
+for (var i = 0; i <= 100; i++) {
+   console.log(i);
+
+	if (i % 3 === 0) {
+		console.log ('Fizz');
+	}
+
+	if (i % 5 == 0) {
+		console.log ('Buzz');
+	}
+
+	if ((i % 3 == 0) && (i % 5 == 0)) {
+		console.log ('FizzBuzz');
+	}
+
+}
+
+//how to string and  say hide. fizz/buzz needs to replace number
+//how to use the else statement
+
+//possibles
+//var i = document.getElementById ('i');
+
+//document.getElementById ('').removeAttribute('');
+
+//var removed = 
+
+//console.log(removed);

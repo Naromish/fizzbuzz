ab2fd61ab60a31421ee6d0bb2e0108fb9b5112a3
Naromish <danermc@gmail.com>
2015-06-07 15:18:31 -0400

    fb one

--- js/fb.js
+++ js/fb.js
@@ -1,32 +1,15 @@
 //$(document).ready(function() {
 
 //});
-
 for (var i = 0; i <= 100; i++) {
-   console.log(i);
 
-	if (i % 3 === 0) {
-		console.log ('Fizz');
-	}
-
-	if (i % 5 == 0) {
-		console.log ('Buzz');
-	}
-
-	if ((i % 3 == 0) && (i % 5 == 0)) {
+   	if ((i % 3 === 0) && (i % 5 === 0)) {
 		console.log ('FizzBuzz');
+	} else if (i % 3 === 0) {
+		console.log ('Fizz');
+	} else if (i % 5 === 0) {
+		console.log ('Buzz');
+	} else  {
+		console.log (i);
 	}
-
 }
-
-//how to string and  say hide. fizz/buzz needs to replace number
-//how to use the else statement
-
-//possibles
-//var i = document.getElementById ('i');
-
-//document.getElementById ('').removeAttribute('');
-
-//var removed = 
-
-//console.log(removed);
